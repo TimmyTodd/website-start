abe915263ce4da6d02204e2fd484e2e382604267
Todd Carpenter <toddbc@gmail.com>
2015-02-18 13:23:43 -0500

    gitgo update

--- admin/gruntfile.js
+++ admin/gruntfile.js
@@ -1,8 +1,11 @@
 module.exports = function(grunt) {
- 
+
+  grunt.registerTask('default', [ 'concat:js', 'uglify:js', 'sass:style', 'cssmin:css' ]);
   grunt.registerTask('watch', [ 'watch' ]);
+  grunt.registerTask('test', ['jshint']);
  
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     concat: {
       js: {
         options: {
@@ -11,24 +14,41 @@ module.exports = function(grunt) {
         src: [
           '../javascript/*.js'
         ],
-        dest: '../public/js/main.min.js'
+        dest: '../public/js/<%= pkg.name %>.min.js'
       },
     },
     uglify: {
       options: {
-        mangle: false
+        mangle: false,
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("mm-dd-yyyy") %> */\n'
       },
       js: {
         files: {
-          '../public/js/main.min.js': ['../public/js/main.min.js']
+          '../public/js/<%= pkg.name %>.min.js': ['../public/js/<%= pkg.name %>.min.js']
+        }
+      }
+    },
+    jshint: {
+      files: ['gruntfile.js', '../public/js/<%= pkg.name %>.min.js'],
+      options: {
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true
         }
       }
     },
     sass: {
       style: {
         files: {
-          "../public/css/main.css": "../sass/styles.scss"
+          "../public/css/style.css": "../sass/styles.scss"
         }
+      }
+    },
+    cssmin : {
+      css:{
+        src: '../public/css/style.css',
+        dest: '../public/css/style.min.css'
       }
     },
     watch: {
@@ -41,7 +61,7 @@ module.exports = function(grunt) {
       },
       css: {
         files: ['../sass/*.scss'],
-        tasks: ['sass:style'],
+        tasks: ['sass:style', 'cssmin:css'],
         options: {
           livereload: true,
         }
@@ -53,5 +73,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
  
 };
